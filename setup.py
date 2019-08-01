bd70cdf5966f8d5d6c2914a36eaad4a361ab1a1c
Felix Metzner <felix.metzner@kit.edu>
2019-08-01 10:34:44 +0200

    Improving setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="TemplateFitter",
     version="0.0.1",
     author="Maximilian Welsch",
     url="https://github.com/welschma/TemplateFitter",
-    packages=[
-        "templatefitter"
-    ],
+    packages=find_packages(),
     description="Perform extended binnend log-likelhood fits using histogram templates as pdfs.",
 )
