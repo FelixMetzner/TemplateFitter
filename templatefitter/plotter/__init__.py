b1c0ced47deb875e381148cd5ec6905729a8aa6d
Felix Metzner <felix.metzner@kit.edu>
2019-11-13 13:51:58 +0100

    Started to work on plotting tools.

---
+++ templatefitter/plotter/__init__.py
@@ -0,0 +1,5 @@
+from templatefitter.plotter.histogram import *
+from templatefitter.plotter.histogram_component import *
+from templatefitter.plotter.histogram_layer import *
+from templatefitter.plotter.histogram_variable import *
+from templatefitter.plotter.plot_style import *
