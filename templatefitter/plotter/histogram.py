925a72fe829399550f3bea9547d30baef6398089
Felix Metzner <felix.metzner@kit.edu>
2019-11-14 17:18:33 +0100

    Improvements of Histogram and HistComponent for plotter part of package.

--- templatefitter/plotter/histogram.py
+++ templatefitter/plotter/histogram.py
@@ -7,9 +7,11 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
-from typing import List, Union
+from typing import Union, List, Tuple
 
 from templatefitter.binned_distributions.binning import Binning
+from templatefitter.binned_distributions import distributions_utility
+from templatefitter.binned_distributions.binned_distribution import BinnedDistribution
 
 from templatefitter.plotter import plot_style
 from templatefitter.plotter.histogram_variable import HistVariable
@@ -42,11 +44,13 @@ class Histogram:
             bins=variable.n_bins,
             dimensions=1,
             scope=variable.scope,
-            log_scale=variable.use_log_scale,
+            log_scale=variable.use_log_scale
         )
 
         self._components = []  # type: List[HistComponent]
         self._auto_color_index = 0
+
+        self._raw_data_scope = None
 
     def add_histogram_component(self, *args, **kwargs) -> None:
         try:
@@ -70,19 +74,42 @@ class Histogram:
     def add_histogram_component_directly(self, component: HistComponent):
         if component.color is None:
             component.color = self._get_auto_color()
+
+        if component.input_column_name is None:
+            component.input_column_name = self.variable.df_label
+        else:
+            assert component.input_column_name == self.variable.df_label, \
+                (component.input_column_name, self.variable.df_label)
+
         self._components.append(component)
 
     def add_histogram_component_via_constructor(self, *args, **kwargs):
         new_component = HistComponent(*args, **kwargs)
         self.add_histogram_component_directly(component=new_component)
 
-    def _find_range_from_components(self):
-        # TODO
-        pass
+    def reset_binning_to_use_raw_data_scope(self):
+        new_binning = Binning(
+            bins=self.binning.bin_edges,
+            dimensions=self.binning.dimensions,
+            scope=self.raw_data_range,
+            log_scale=self.variable.use_log_scale
+        )
+        self._binning = new_binning
 
-    def _get_adapted_binning(self):
-        # TODO
-        pass
+    def apply_adapted_binning(self, minimal_bin_count: int = 5, minimal_number_of_bins: int = 7):
+        new_bin_edges = distributions_utility.run_adaptive_binning(
+            distributions=self._get_underlying_distributions(),
+            bin_edges=self.binning.bin_edges,
+            minimal_bin_count=minimal_bin_count,
+            minimal_number_of_bins=minimal_number_of_bins,
+        )
+        new_binning = Binning(
+            bins=new_bin_edges,
+            dimensions=self.binning.dimensions,
+            scope=self.binning.range,
+            log_scale=self.variable.use_log_scale
+        )
+        self._binning = new_binning
 
     def get_bin_counts(self) -> List[np.ndarray]:
         return [component.get_histogram_bin_count(binning=self.binning) for component in self._components]
@@ -115,6 +142,37 @@ class Histogram:
             return bin_widths[0][0]
         else:
             return np.array(bin_widths[0])
+
+    @property
+    def raw_data_range(self) -> Tuple[float, float]:
+        return self._find_range_from_components()
+
+    def _get_underlying_distributions(self) -> List[BinnedDistribution]:
+        return [component.get_underlying_binned_distribution(binning=self.binning) for component in self._components]
+
+    def _find_range_from_components(self) -> Tuple[float, float]:
+        if self._raw_data_scope is not None:
+            return self._raw_data_scope
+
+        min_values = []
+        max_values = []
+
+        if len(self._components) == 0:
+            raise RuntimeError("No components available to derive raw data range from...")
+
+        for component in self._components:
+            if component.input_column_name is None:
+                component.input_column_name = self.variable.df_label
+            else:
+                assert component.input_column_name == self.variable.df_label, \
+                    (component.label, component.input_column_name, self.variable.df_label)
+
+            min_values.append(np.amin(component.min_val))
+            max_values.append(np.amax(component.max_val))
+
+        scope_tuple = (np.amin(min_values), np.amax(max_values))
+        self._raw_data_scope = scope_tuple
+        return scope_tuple
 
     def _get_auto_color(self):
         colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
--- templatefitter/plotter/histogram_component.py
+++ templatefitter/plotter/histogram_component.py
@@ -2,9 +2,11 @@
 Provides the HistComponent class, which is a container combining a BinnedDistribution with
 information necessary to plot it, such as the label and color of the component in the plot.
 """
-import numpy as np
 
-from typing import Optional
+import numpy as np
+import pandas as pd
+
+from typing import Optional, Tuple
 
 from templatefitter.binned_distributions.binning import Binning
 from templatefitter.binned_distributions.weights import WeightsInputType
@@ -48,12 +50,15 @@ class HistComponent:
         self._input_data = data
         self._input_weights = weights
         self._input_systematics = systematics
-        self._input_column_names = data_column_names
+        self._input_column_name = self._get_data_column_name(data_column_names=data_column_names)
 
         self._label = label
         self._hist_type = hist_type
         self._color = color
         self._alpha = alpha
+
+        self._min_val = None
+        self._max_val = None
 
         self._binned_distribution = None
 
@@ -63,12 +68,19 @@ class HistComponent:
         :param binning: The binning to be used to generate the histogram and calculate the bin count.
         :return: A np.ndarray containing the bin counts.
         """
+        binned_dist = self.get_underlying_binned_distribution(binning=binning)
+        bin_count = binned_dist.bin_counts
+        assert bin_count is not None
+        return bin_count
+
+    def get_underlying_binned_distribution(self, binning: Binning) -> BinnedDistribution:
         if self._binned_distribution is not None:
-            assert isinstance(self._binned_distribution, BinnedDistribution), type(self._binned_distribution)
-            if self._binned_distribution.binning == binning:
-                bin_count = self._binned_distribution.bin_counts
-                assert bin_count is not None
-                return bin_count
+            if binning == self._binned_distribution.binning:
+                return self._binned_distribution
+
+        assert self.input_data is not None
+        if isinstance(self.input_data, pd.DataFrame):
+            assert self.input_column_name is not None
 
         binned_dist = BinnedDistribution(
             bins=binning.bin_edges,
@@ -77,13 +89,13 @@ class HistComponent:
             data=self.input_data,
             weights=self.input_weights,
             systematics=self.input_systematics,
-            data_column_names=self.input_column_names
+            data_column_names=self.input_column_name
         )
 
         assert isinstance(binned_dist, BinnedDistribution), type(binned_dist)
         assert not binned_dist.is_empty
         self._binned_distribution = binned_dist
-        return binned_dist.bin_counts
+        return binned_dist
 
     @property
     def label(self) -> str:
@@ -93,17 +105,41 @@ class HistComponent:
     def input_data(self) -> DataInputType:
         return self._input_data
 
+    @input_data.setter
+    def input_data(self, input_data: DataInputType) -> None:
+        if self._input_data is not None:
+            raise RuntimeError("You are trying to overwrite the HistogramComponents input_data!")
+        self._input_data = input_data
+
     @property
     def input_weights(self) -> WeightsInputType:
         return self._input_weights
+
+    @input_weights.setter
+    def input_weights(self, input_weights: WeightsInputType) -> None:
+        if self._input_weights is not None:
+            raise RuntimeError("You are trying to overwrite the HistogramComponents input_weights!")
+        self._input_weights = input_weights
 
     @property
     def input_systematics(self) -> SystematicsInputType:
         return self._input_systematics
 
+    @input_systematics.setter
+    def input_systematics(self, input_systematics: SystematicsInputType) -> None:
+        if self._input_systematics is not None:
+            raise RuntimeError("You are trying to overwrite the HistogramComponents input_systematics!")
+        self._input_systematics = input_systematics
+
     @property
-    def input_column_names(self) -> DataColumnNamesInput:
-        return self._input_column_names
+    def input_column_name(self) -> Optional[str]:
+        return self._input_column_name
+
+    @input_column_name.setter
+    def input_column_name(self, input_column_names: DataColumnNamesInput) -> None:
+        if self._input_column_name is not None:
+            raise RuntimeError("You are trying to overwrite the HistogramComponents input_column_names!")
+        self._input_column_name = self._get_data_column_name(data_column_names=input_column_names)
 
     @property
     def hist_type(self) -> Optional[str]:
@@ -122,19 +158,39 @@ class HistComponent:
         return self._alpha
 
     @property
-    def min_val(self) -> Optional[float]:
-        if self._binned_distribution is None:
-            return None
-        histogram_range = self._binned_distribution.binning.range
-        assert len(histogram_range) == 1, (len(histogram_range), len(histogram_range))
-        assert len(histogram_range[0]) == 2, (len(histogram_range[0]), histogram_range[0])
-        return histogram_range[0][0]
+    def raw_data_range(self) -> Tuple[float, float]:
+        if self._min_val is not None and self._max_val is not None:
+            return self._min_val, self._max_val
+
+        if isinstance(self.input_data, pd.DataFrame) and self.input_column_name is None:
+            raise RuntimeError("input_column_name must be defined if input_data is a pandas.DataFrame!")
+
+        raw_data = BinnedDistribution.get_data_input(in_data=self.input_data, data_column_names=self.input_column_name)
+        assert len(raw_data.shape) == 1, raw_data.shape
+
+        self._min_val = np.amin(raw_data) if len(raw_data) > 0 else +float("inf")
+        self._max_val = np.amax(raw_data) if len(raw_data) > 0 else -float("inf")
+
+        return self._min_val, self._max_val
 
     @property
-    def max_val(self) -> Optional[float]:
-        if self._binned_distribution is None:
-            return None
-        histogram_range = self._binned_distribution.binning.range
-        assert len(histogram_range) == 1, (len(histogram_range), len(histogram_range))
-        assert len(histogram_range[0]) == 2, (len(histogram_range[0]), histogram_range[0])
-        return histogram_range[0][1]
+    def min_val(self) -> float:
+        return self.raw_data_range[0]
+
+    @property
+    def max_val(self) -> float:
+        return self.raw_data_range[1]
+
+    @staticmethod
+    def _get_data_column_name(data_column_names: DataColumnNamesInput) -> Optional[str]:
+        if isinstance(data_column_names, str):
+            return data_column_names
+        elif isinstance(data_column_names, list):
+            assert len(data_column_names) == 1, (len(data_column_names), data_column_names)
+            assert isinstance(data_column_names[0], str), data_column_names
+            return data_column_names[0]
+        elif data_column_names is None:
+            return data_column_names
+        else:
+            raise TypeError(f"data_column_names must be either a string, a list containing ONE string, or None.\n"
+                            f"You provided {data_column_names}")
