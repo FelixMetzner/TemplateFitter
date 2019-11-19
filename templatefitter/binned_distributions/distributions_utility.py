Fixing returning unity matrix instead of zeros for no systematics and updated handlig of empty covariance matrix in toy_chi2_test.

templatefitter/binned_distributions/distributions_utility.py
@@ -33,7 +33,7 @@ def get_combined_covariance(distributions: DistributionContainerInputType) -> np
         ([len(d.systematics) for d in distributions])
 
     if all(len(dist.systematics) == 0 for dist in distributions):
-        return np.eye(common_binning.num_bins_total)
+        return np.zeros((common_binning.num_bins_total, common_binning.num_bins_total))
 
     cov = np.zeros((common_binning.num_bins_total, common_binning.num_bins_total))
 

templatefitter/stats.py
@@ -191,7 +191,8 @@ def mc_chi_squared_from_toys(
     obs_chi_squared = calc_chi_squared(obs, exp, exp_unc)
 
     np.random.seed(seed=seed)
-    if mc_cov is None:
+    if mc_cov is None or not np.any(mc_cov):
+        # if covariance matrix is None or contains only zeros (checked with "not np.any(mc_cov)"): use poisson approach
         toys = np.random.poisson(exp, size=(toys_size, len(exp)))
     else:
         toys = np.random.multivariate_normal(mean=exp, cov=mc_cov, size=toys_size)