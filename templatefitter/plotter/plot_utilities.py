9341486087e73337cae2bd153b16084b8aea4616
Felix Metzner <felix.metzner@kit.edu>
2020-02-18 18:12:17 +0100

    Adding option to close figure after saving them with the export method.

--- templatefitter/plotter/plot_utilities.py
+++ templatefitter/plotter/plot_utilities.py
@@ -21,7 +21,8 @@ def export(
         fig: plt.Figure,
         filename: Union[str, os.fspath],
         target_dir: str = "plots/",
-        file_formats: Tuple[str] = (".pdf", ".png")
+        file_formats: Tuple[str] = (".pdf", ".png"),
+        close_figure: bool = False
 ) -> None:
     """
     Convenience function for saving a matplotlib figure.
@@ -29,15 +30,20 @@ def export(
     :param fig: A matplotlib figure.
     :param filename: Filename of the plot without .pdf suffix.
     :param file_formats: Tuple of file formats specifying the format
-    figure will be saved as.
+                         figure will be saved as.
     :param target_dir: Directory where the plot will be saved in.
-    Default is './plots/'.
+                       Default is './plots/'.
+    :param close_figure: Whether to close the figure after saving it.
+                         Default is False
     :return: None
     """
     os.makedirs(target_dir, exist_ok=True)
 
     for file_format in file_formats:
         fig.savefig(os.path.join(target_dir, f'{filename}{file_format}'), bbox_inches="tight")
+
+    if close_figure:
+        plt.close(fig)
 
 
 def color_fader(color_1: str, color_2: str, mix: float = 0.) -> str:
