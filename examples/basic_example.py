c4c32c5e62f8975d01d451603e37925eabb3223a
Felix Metzner <felixmetzner@outlook.com>
2022-09-23 17:07:40 +0200

    Adding / fixing some typehints and some minor changes in the sig and bkg event generation functions of the example.

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-""""
+"""
 This is a basic example of a template fit with two templates in a single fit dimension and channel.
 """
 
 import numpy as np
 import pandas as pd
-from templatefitter.fit_model.model_builder import FitModel
-from templatefitter.fit_model.parameter_handler import ParameterHandler
-from templatefitter.fit_model.template import Template
-from templatefitter.fitter import TemplateFitter
-from templatefitter.plotter.fit_result_plots import FitResultPlotter
-from templatefitter.plotter.histogram_variable import HistVariable
-from templatefitter.plotter.plot_style import KITColors
 
 from pathlib import Path
+from typing import Tuple, Dict
+
+from templatefitter.fit_model.template import Template
+from templatefitter.fit_model.model_builder import FitModel
+from templatefitter.fit_model.parameter_handler import ParameterHandler
+
+from templatefitter.fitter import TemplateFitter
+from templatefitter.minimizer import MinimizeResult
+
+from templatefitter.plotter.plot_style import KITColors
+from templatefitter.plotter.histogram_variable import HistVariable
+from templatefitter.plotter.fit_result_plots import FitResultPlotter
 
 
-def gen_sig(size, voi, voi_limits):
-    df = pd.DataFrame(
-        {voi: np.random.normal((voi_limits[1] + voi_limits[0]) / 2, 1, int(size)), "weight": [1.0] * int(size)}
-    )
+def gen_sig(size: int, voi: str, voi_limits: Tuple[float, float]) -> pd.DataFrame:
+    variable_values = np.random.normal((voi_limits[1] + voi_limits[0]) / 2, 1, int(size))
+    df = pd.DataFrame({voi: variable_values, "weight": np.ones_like(variable_values)})
     df.name = "signal"
     return df
 
 
-def gen_bkg(size, voi, voi_limits):
-    df = pd.DataFrame(
-        {
-            voi: voi_limits[0] + (voi_limits[1] - voi_limits[0]) * np.random.random_sample(int(size)),
-            "weight": [1.0] * int(size),
-        }
-    )
+def gen_bkg(size: int, voi: str, voi_limits: Tuple[float, float]) -> pd.DataFrame:
+    variable_values = voi_limits[0] + (voi_limits[1] - voi_limits[0]) * np.random.random_sample(int(size))
+    df = pd.DataFrame({voi: variable_values, "weight": np.ones_like(variable_values)})
     df.name = "background"
     return df
 
 
-def run_basic_example():
+def run_basic_example() -> Tuple[MinimizeResult, Dict[str, float]]:
 
     # Defining a variable and range for which we will generate data
-    voi = "variableOfInterest"
-    voi_physics_limits = (0, 20)
+    voi = "variableOfInterest"  # type: str
+    voi_physics_limits = (0, 20)  # type: Tuple[float, float]
 
-    # Defining a HistVariable object for the fit to use. Note: The scope / fit range used here can be (and is) smaller
-    # then the one used for generating the dataset.
+    # Defining a HistVariable object for the fit to use.
+    # Note: The scope / fit range used here can be (and is) smaller than the one used for generating the dataset.
     # This is done for illustration here but can sometimes be beneficial.
-    # The fitter will later correct the yields so they apply to the entire dataset.
+    # The fitter will later correct the yields, so they apply to the entire dataset.
     fit_var = HistVariable(df_label=voi, n_bins=10, scope=(5, 10), var_name="Variable of Interest")
 
     # Defining a decay channel and two components from which templates will be generated
@@ -65,7 +65,8 @@ def run_basic_example():
         for component in components
     }
 
-    # Defining a ParameterHandler which holds all model parameters and a FitModel object which describes the rest of the fit
+    # Defining a ParameterHandler which holds all model parameters
+    # and a FitModel object which describes the rest of the fit
     param_handler = ParameterHandler()
     model = FitModel(parameter_handler=param_handler)
 
@@ -120,8 +121,8 @@ def run_basic_example():
     # Lastly we'll finalize the model. This finalized model could now be saved with Python's pickle serializer.
     model.finalize_model()
 
-    # Now that we're done creating the model, we can see how data and MC agreed before the fit. Agreement should be perfect
-    # in a fit to Asimov data.
+    # Now that we're done creating the model, we can see how data and MC agreed before the fit.
+    # Agreement should be perfect in a fit to Asimov data.
     # To do this, we first have to set up a FitResultPlotter.
 
     output_folder = Path("./plots")
@@ -134,20 +135,24 @@ def run_basic_example():
     # Here we'll now set up a TemplateFitter object and perform the fit itself.
     # We'll use nuisance parameter for each bin and each template in the model.
     fitter = TemplateFitter(fit_model=model, minimizer_id="iminuit")
-    fit_result = fitter.do_fit(update_templates=True, get_hesse=True, verbose=False, fix_nui_params=False)
+    result = fitter.do_fit(update_templates=True, get_hesse=True, verbose=False, fix_nui_params=False)
 
     # Now let's plot the fit results
     fit_result_plotter_m2.plot_fit_result(output_dir_path=output_folder, output_name_tag="")
 
-    # Lastly, we can have a look at the significance of the signal + background hypothesis vs. a background only hypothesis.
+    # Lastly, we can have a look at the significance of the signal + background hypothesis
+    # vs. the background only hypothesis.
     # Note: You might get a warning from numpy which you can safely ignore here.
     significance_dict = {}
     for yield_parameter in param_handler.get_parameter_names_for_type(ParameterHandler.yield_parameter_type):
         significance_dict[yield_parameter] = fitter.get_significance(
-            yield_parameter=yield_parameter, fix_nui_params=False, verbose=False, catch_exception=True
+            yield_parameter=yield_parameter,
+            fix_nui_params=False,
+            verbose=False,
+            catch_exception=True,
         )
 
-    return fit_result, significance_dict
+    return result, significance_dict
 
 
 if __name__ == "__main__":
@@ -156,5 +161,6 @@ if __name__ == "__main__":
     print(f"The result of the fit is: \n {str(fit_result.params)} \n")
 
     print(
-        f"The significance of the signal + background hypothesis vs. only background is {sign_dict['yield_signal']:.1f} sigma."
+        f"The significance of the signal + background hypothesis vs. only background "
+        f"is {sign_dict['yield_signal']:.1f} sigma."
     )
