Fixed off-by-one bug in slice determination and added assert to catch similar issues

templatefitter/fit_model/model_builder.py
@@ -780,11 +780,10 @@ class FitModel:
         bin_nuisance_param_indices = self._params.get_parameter_indices_for_type(
             parameter_type=ParameterHandler.bin_nuisance_parameter_type,
         )
+        self.bin_nuisance_parameter_slice = create_slice_if_contiguous(bin_nuisance_param_indices)
 
         if not self._bin_nuisance_params_checked:
             self._check_bin_nuisance_parameters()
-
-        self.bin_nuisance_parameter_slice = create_slice_if_contiguous(bin_nuisance_param_indices)
 
         return np.array(bin_nuisance_param_indices)
 
@@ -1149,6 +1148,18 @@ class FitModel:
 
         constraint_indices = [c.constraint_index for c in self._constraint_container]
         self.constraint_slice = create_slice_if_contiguous(constraint_indices)
+        if self.constraint_slice is not None:
+
+            constr_param_values = self._params.get_parameters_by_index(constraint_indices)
+            if isinstance(constr_param_values, float):
+                assert len(self._params.get_parameters_by_slice(self.constraint_slice)) == 1
+                assert all([constr_param_values] == self._params.get_parameters_by_slice(self.constraint_slice))
+            else:
+                assert len(self._params.get_parameters_by_slice(self.constraint_slice)) == len(constr_param_values)
+                assert all(
+                    self._params.get_parameters_by_index(constraint_indices)
+                    == self._params.get_parameters_by_slice(self.constraint_slice)
+                )
 
         return np.array(constraint_indices)
 
@@ -1567,6 +1578,19 @@ class FitModel:
                             ],
                         )
                         index_counter += n_bins
+
+        if self.bin_nuisance_parameter_slice is not None:
+
+            nui_param_values = self._params.get_parameters_by_index(nu_is)
+            if isinstance(nui_param_values, float):
+                assert len(self._params.get_parameters_by_slice(self.bin_nuisance_parameter_slice)) == 1
+                assert all([nui_param_values] == self._params.get_parameters_by_slice(self.bin_nuisance_parameter_slice))
+
+            else:
+                assert len(self._params.get_parameters_by_slice(self.bin_nuisance_parameter_slice)) == len(
+                    nui_param_values
+                )
+                assert all(nui_param_values == self._params.get_parameters_by_slice(self.bin_nuisance_parameter_slice))
 
         self._bin_nuisance_params_checked = True
 