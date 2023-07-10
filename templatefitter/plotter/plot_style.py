ea04eaee16d1bb59bb54c048b495bee604f8927d
Felix Metzner <felixmetzner@outlook.com>
2023-07-10 12:08:48 +0200

    Adding Tango colors.

--- templatefitter/plotter/plot_style.py
+++ templatefitter/plotter/plot_style.py
@@ -28,22 +28,22 @@ class KITColors:
     KIT color scheme plus additional grey shades
     """
 
-    kit_green = "#009682"  # type: str
-    kit_blue = "#4664aa"  # type: str
-    kit_maygreen = "#8cb63c"  # type: str
-    kit_yellow = "#fce500"  # type: str
-    kit_orange = "#df9b1b"  # type: str
-    kit_brown = "#a7822e"  # type: str
-    kit_red = "#a22223"  # type: str
-    kit_purple = "#a3107c"  # type: str
-    kit_cyan = "#23a1e0"  # type: str
-    kit_black = "#000000"  # type: str
-    white = "#ffffff"  # type: str
-    light_grey = "#bdbdbd"  # type: str
-    grey = "#797979"  # type: str
-    dark_grey = "#4e4e4e"  # type: str
+    kit_green: str = "#009682"
+    kit_blue: str = "#4664aa"
+    kit_maygreen: str = "#8cb63c"
+    kit_yellow: str = "#fce500"
+    kit_orange: str = "#df9b1b"
+    kit_brown: str = "#a7822e"
+    kit_red: str = "#a22223"
+    kit_purple: str = "#a3107c"
+    kit_cyan: str = "#23a1e0"
+    kit_black: str = "#000000"
+    white: str = "#ffffff"
+    light_grey: str = "#bdbdbd"
+    grey: str = "#797979"
+    dark_grey: str = "#4e4e4e"
 
-    default_colors = [
+    default_colors: List[str] = [
         kit_blue,
         kit_orange,
         kit_green,
@@ -54,10 +54,56 @@ class KITColors:
         dark_grey,
         kit_cyan,
         kit_maygreen,
-    ]  # type: List[str]
+    ]
 
 
 kit_color_cycler = cycler("color", KITColors.default_colors)
+
+
+class TangoColors:
+    butter1: str = "#fce94f"
+    butter2: str = "#edd400"
+    butter3: str = "#c4a000"
+    orange1: str = "#fcaf3e"
+    orange2: str = "#f57900"
+    orange3: str = "#ce5c00"
+    chocolate1: str = "#e9b96e"
+    chocolate2: str = "#c17d11"
+    chocolate3: str = "#8f5902"
+    chameleon1: str = "#8ae234"
+    chameleon2: str = "#73d216"
+    chameleon3: str = "#4e9a06"
+    skyblue1: str = "#729fcf"
+    skyblue2: str = "#3465a4"
+    skyblue3: str = "#204a87"
+    plum1: str = "#ad7fa8"
+    plum2: str = "#75507b"
+    plum3: str = "#5c3566"
+    scarletred1: str = "#ef2929"
+    scarletred2: str = "#cc0000"
+    scarletred3: str = "#a40000"
+    aluminium1: str = "#eeeeec"
+    aluminium2: str = "#d3d7cf"
+    aluminium3: str = "#babdb6"
+    aluminium4: str = "#888a85"
+    aluminium5: str = "#555753"
+    aluminium6: str = "#2e3436"
+
+    default_colors: List[str] = [
+        skyblue2,
+        orange2,
+        chameleon3,
+        scarletred2,
+        plum2,
+        chocolate3,
+        butter2,
+        aluminium4,
+        skyblue1,
+        chameleon1,
+    ]
+
+
+tango_color_cycler = cycler("color", TangoColors.default_colors)
 
 
 def set_matplotlibrc_params() -> None:
